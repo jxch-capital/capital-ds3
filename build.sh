7765028a32a09151f8a61f910d9949edc393a091
jiangxicheng <jiangxicheng@msunsoft.com>
2022-08-01 09:40:07 +0800

    shell build docker 支持 arm64

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker buildx build --platform=linux/amd64  -t  jxch/capital-ds3:$(date +%Y%m%d) -t jxch/capital-ds3:latest . --push
+sudo docker buildx build --platform=linux/arm64,linux/amd64  -t  jxch/capital-ds3:$(date +%Y%m%d) -t jxch/capital-ds3:latest . --push
